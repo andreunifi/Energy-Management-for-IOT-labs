972fee78d1842d7d62287bf1f7fcdbfcb490d1a2
Giuko <peppecicciomaganuco13@gmail.com>
2026-02-16 16:00:43 +0100

    Added the trace of PV, expanded the jupyter code to compute and plot MPP, populated inc/config_pv.h with MPP points

--- lab3/simulator/inc/config_pv.h
+++ lab3/simulator/inc/config_pv.h
@@ -1,5 +1,16 @@
 #define TRACE_PERIOD 900
 #define SIZE_PV 4 
-static const double G[SIZE_PV] = {TO-BE-POPULATED};
-static const double I_MPP[SIZE_PV] = {TO-BE-POPULATED};
-static const double V_MPP[SIZE_PV] = {TO-BE-POPULATED};
+
+#define I250  13.1992
+#define I500  28.3300
+#define I750  41.8511
+#define I1000 54.8893
+
+#define V250  3.0020
+#define V500  3.0020
+#define V750  3.1390
+#define V1000 3.2860
+
+static const double G[SIZE_PV] = {250, 500, 750, 100};
+static const double I_MPP[SIZE_PV] = {I250, I500, I750, I1000};
+static const double V_MPP[SIZE_PV] = {V250, V500, V750, V1000};
